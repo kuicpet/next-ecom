69fda059ddbd43c6904a4670779f9a1bed68b66e
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-08-16 15:51:34 +0100

    ft--mongodb added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// import db from "../../utils/db"
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // db.connect()
+  // db.disconnect()
+  res.status(200).json({ name: 'John Doe' });
 }
