14c358c47d39b64c81c12620b2ccff51e44fb8a2
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-08-16 15:05:47 +0100

    ft--context,theme added

--- components/Layout.js
+++ components/Layout.js
@@ -4,40 +4,85 @@ import {
   Link,
   Toolbar,
   Typography,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
 } from '@material-ui/core';
 import Head from 'next/head';
-import React from 'react';
+import React, { useContext } from 'react';
 import NextLink from 'next/link';
 import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 const Layout = ({ title, description, children }) => {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      body1: {
+        fontWeight: 'normal',
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f47036',
+      },
+      secondary: {
+        main: '#2e2e2e',
+      },
+    },
+  });
   const classes = useStyles();
+  const darkModeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF')
+  };
   return (
     <div>
       <Head>
-        <title>{title ? `${title} - Next-Ecom`: 'Next-Ecom'}</title>
+        <title>{title ? `${title} - Next-Ecom` : 'Next-Ecom'}</title>
         {description && <meta name="description" content={description} />}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/">
-            <Link>
-              <Typography className={classes.brand}>Next-Ecom</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/">
+              <Link>
+                <Typography className={classes.brand}>Next-Ecom</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>All rights reserved Next-Ecom</footer>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch checked={darkMode} onChange={darkModeHandler}></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          All rights reserved Next-Ecom
+        </footer>
+      </ThemeProvider>
     </div>
   );
 };
---
+++ utils/Store.js
@@ -0,0 +1,30 @@
+import Cookies from 'js-cookie';
+import { createContext, useReducer } from 'react';
+
+export const Store = createContext();
+const initialStore = {
+  darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'DARK_MODE_ON':
+      return {
+        ...state,
+        darkMode: true,
+      };
+    case 'DARK_MODE_OFF':
+      return {
+        ...state,
+        darkMode: false,
+      };
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialStore);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+}
