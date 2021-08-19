c8c114ef83bdcc5426d7fc1ad502c14a5b3e71db
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-08-19 01:07:57 +0100

    ft--form validation added to login form using react-hook-form

--- pages/api/users/register.js
+++ pages/api/users/register.js
@@ -1,38 +1,40 @@
 import {
-  Button,
   List,
   ListItem,
-  TextField,
   Typography,
+  TextField,
+  Button,
   Link,
 } from '@material-ui/core';
-import React, { useContext, useEffect, useState } from 'react';
-import NextLink from 'next/link';
-import Layout from '../components/Layout';
-import useStyles from '../utils/styles';
+import axios from 'axios';
 import { useRouter } from 'next/router';
+import NextLink from 'next/link';
+import React, { useContext, useEffect, useState } from 'react';
+import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
+import useStyles from '../utils/styles';
+import Cookies from 'js-cookie';
 
-const Register = () => {
+export default function Register() {
   const router = useRouter();
-  const redirect = router.query;
-  const classes = useStyles();
+  const { redirect } = router.query;
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
- useEffect(() => {
-  if (userInfo) {
-    router.push('/');
-  }
- },[])
+  useEffect(() => {
+    if (userInfo) {
+      router.push('/');
+    }
+  }, []);
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
+  const classes = useStyles();
   const submitHandler = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      alert('Passwords do not match');
+      alert("passwords don't match");
       return;
     }
     try {
@@ -42,25 +44,24 @@ const Register = () => {
         password,
       });
       dispatch({ type: 'USER_LOGIN', payload: data });
-      console.log(data);
       Cookies.set('userInfo', data);
       router.push(redirect || '/');
-    } catch (error) {
-      alert(error.response.data ? error.response.data.message : error.message);
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
     }
   };
   return (
     <Layout title="Register">
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Register
         </Typography>
         <List>
           <ListItem>
             <TextField
-              id="name"
-              fullWidth
               variant="outlined"
+              fullWidth
+              id="name"
               label="Name"
               inputProps={{ type: 'text' }}
               onChange={(e) => setName(e.target.value)}
@@ -68,9 +69,9 @@ const Register = () => {
           </ListItem>
           <ListItem>
             <TextField
-              id="email"
-              fullWidth
               variant="outlined"
+              fullWidth
+              id="email"
               label="Email"
               inputProps={{ type: 'email' }}
               onChange={(e) => setEmail(e.target.value)}
@@ -78,9 +79,9 @@ const Register = () => {
           </ListItem>
           <ListItem>
             <TextField
+              variant="outlined"
               fullWidth
               id="password"
-              variant="outlined"
               label="Password"
               inputProps={{ type: 'password' }}
               onChange={(e) => setPassword(e.target.value)}
@@ -88,35 +89,27 @@ const Register = () => {
           </ListItem>
           <ListItem>
             <TextField
+              variant="outlined"
               fullWidth
               id="confirmPassword"
-              variant="outlined"
               label="Confirm Password"
               inputProps={{ type: 'password' }}
               onChange={(e) => setConfirmPassword(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
-            <Button
-              variant="contained"
-              type="submit"
-              fullWidth
-              color="primary"
-              className={classes.button}
-            >
+            <Button variant="contained" type="submit" fullWidth color="primary">
               Register
             </Button>
           </ListItem>
           <ListItem>
-            Have an Account? &nbsp;
-            <NextLink href={`/login?redirect=${redirect} || '/`} passHref>
-              <Link> Login</Link>
+            Already have an account? &nbsp;
+            <NextLink href={`/login?redirect=${redirect || '/'}`} passHref>
+              <Link>Login</Link>
             </NextLink>
           </ListItem>
         </List>
       </form>
     </Layout>
   );
-};
-
-export default Register;
+}
--- pages/login.js
+++ pages/login.js
@@ -14,61 +14,110 @@ import axios from 'axios';
 import { Store } from '../utils/Store';
 import { useRouter } from 'next/dist/client/router';
 import Cookies from 'js-cookie';
+import { Controller, useForm } from 'react-hook-form';
+import { useSnackbar } from 'notistack';
 
 const Login = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const router = useRouter();
   const { redirect } = router.query;
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
- useEffect(() => {
-  if (userInfo) {
-    router.push('/');
-  }
- },[])
-  const [email, setEmail] = useState('');
-  const [password, setpassword] = useState('');
+  useEffect(() => {
+    if (userInfo) {
+      router.push('/');
+    }
+  }, []);
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
   const classes = useStyles();
-  const submitHandler = async (e) => {
-    e.preventDefault();
+  const submitHandler = async ({ email, password }) => {
+    closeSnackbar();
     try {
       const { data } = await axios.post('/api/users/login', {
         email,
         password,
       });
       dispatch({ type: 'USER_LOGIN', payload: data });
-      console.log(data)
+      console.log(data);
       Cookies.set('userInfo', data);
       router.push(redirect || '/');
     } catch (error) {
-      alert(error.response.data ? error.response.data.message : error.message);
+      enqueueSnackbar(
+        error.response.data ? error.response.data.message : error.message,
+        { variant: 'error' }
+      );
     }
   };
   return (
     <Layout title="Login">
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form className={classes.form} onSubmit={handleSubmit(submitHandler)}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
         <List>
           <ListItem>
-            <TextField
-              id="email"
-              fullWidth
-              variant="outlined"
-              label="Email"
-              inputProps={{ type: 'email' }}
-              onChange={(e) => setEmail(e.target.value)}
-            ></TextField>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              }}
+              render={({ field }) => (
+                <TextField
+                  id="email"
+                  fullWidth
+                  variant="outlined"
+                  label="Email"
+                  inputProps={{ type: 'email' }}
+                  error={Boolean(errors.email)}
+                  helperText={
+                    errors.email
+                      ? errors.email.type === 'pattern'
+                        ? 'Email is not valid'
+                        : 'Email is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              fullWidth
-              id="password"
-              variant="outlined"
-              label="Password"
-              inputProps={{ type: 'password' }}
-              onChange={(e) => setpassword(e.target.value)}
-            ></TextField>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 6,
+              }}
+              render={({ field }) => (
+                <TextField
+                  id="password"
+                  fullWidth
+                  variant="outlined"
+                  label="Password"
+                  inputProps={{ type: 'password' }}
+                  error={Boolean(errors.password)}
+                  helperText={
+                    errors.password
+                      ? errors.password.type === 'minLength'
+                        ? 'Password length is more than 5'
+                        : 'Password is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
             <Button
@@ -83,7 +132,7 @@ const Login = () => {
           </ListItem>
           <ListItem>
             Don't have an Account? &nbsp;{' '}
-            <NextLink href="/register" passHref>
+            <NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
               <Link>Register</Link>
             </NextLink>
           </ListItem>
