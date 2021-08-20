be62974f7b3030d42937d9d22da2aeda72218714
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-08-20 16:49:55 +0100

    ft--order api created

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
 
 const signToken = (user) => {
   return jwt.sign(
@@ -15,4 +15,21 @@ const signToken = (user) => {
   );
 };
 
-export { signToken };
+const isAuth = (req, res) => {
+  const { authorization } = req.headers;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Token is not valid' });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: 'Token is not supplied' });
+  }
+};
+
+export { signToken, isAuth };
--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,12 @@
+import db from './db';
+
 const getError = (error) =>
   error.response && error.response.data && error.response.data.message
     ? error.response.data.message
     : error.message;
 
-export { getError };
+const onError = async (err, req, res, next) => {
+  await db.disconnect();
+  res.status(500).send({ message: err.toString()});
+};
+export { getError, onError };
