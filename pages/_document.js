be6dc14defc0abc951a9adb0db9a4c920c613725
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-08-22 15:52:06 +0100

    ft--updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,35 +1,35 @@
-import { ServerStyleSheets } from '@material-ui/core/styles'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import React from 'react'
-
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
 
 export default class myDocument extends Document {
-    render(){
-        return (
-            <Html lang='en'>
-                <Head></Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 myDocument.getInitialProps = async (ctx) => {
-    const sheets = new ServerStyleSheets()
-    const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () => {
-       return  originalRenderPage({
-            enhanceApp: (App) => (props) => sheets.collect(<App {...props}/>)
-        })
-    }
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-        ...initialProps,
-        styles: [
-            ...React.Children.toArray(initialProps.styles), sheets.getStyleElement()
-        ]
-    }
-}
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
